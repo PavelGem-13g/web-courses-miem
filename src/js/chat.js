1a05de4f4ea9a437f5e3097a376f08700ca52d1e
hazkerindii228 <59193313+PavelGem-13g@users.noreply.github.com>
2023-02-23 01:02:12 +0300

    add chat

---
+++ src/js/chat.js
@@ -0,0 +1,11 @@
+let messagebox = document.querySelector("#messagebox")
+let sendbutton = document.querySelector("#sendbutton")
+let textchat = document.querySelector("#textchat")
+console.log(textchat)
+
+sendbutton.onclick = (event) =>{
+  let message = document.createElement("p");
+  textchat = document.querySelector("#textchat");
+  message.innerHTML = textchat.value;
+  messagebox.append(message); 
+}
