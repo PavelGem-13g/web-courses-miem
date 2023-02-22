6e310d0480f07ab654610e69d1fc47e29f0d7725
hazkerindii228 <59193313+PavelGem-13g@users.noreply.github.com>
2023-02-23 01:37:47 +0300

    add working chat

--- src/js/chat.js
+++ src/js/chat.js
@@ -1,11 +1,14 @@
-let messagebox = document.querySelector("#messagebox")
-let sendbutton = document.querySelector("#sendbutton")
-let textchat = document.querySelector("#textchat")
-console.log(textchat)
+window.onload = function () {
+    let messagebox = document.querySelector("#messagebox")
+    let sendbutton = document.querySelector("#sendbutton")
+    let textchat = document.querySelector("#textchat")
+    console.log(textchat)
 
-sendbutton.onclick = (event) =>{
-  let message = document.createElement("p");
-  textchat = document.querySelector("#textchat");
-  message.innerHTML = textchat.value;
-  messagebox.append(message); 
+    sendbutton.onclick = (event) => {
+        let message = document.createElement("p");
+        textchat = document.querySelector("#textchat");
+        message.innerHTML = textchat.value;
+        message.classList.add("message")
+        messagebox.append(message);
+    }
 }
--- src/js/map.js
+++ src/js/map.js
@@ -1,9 +1,11 @@
-var map = L.map('mapid').setView([55.8034621,37.4077102], 15);
+window.onload = function () {
+    var map = L.map('mapid').setView([55.8034621, 37.4077102], 15);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
 
-L.marker([55.8034302,37.4099802]).addTo(map)
-    .bindPopup('МИЭМ')
-    .openPopup();
+    L.marker([55.8034302, 37.4099802]).addTo(map)
+        .bindPopup('МИЭМ')
+        .openPopup();
+}
